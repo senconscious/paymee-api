9924c0e7aa5dcf570c55322d3597af60eeb8f450
Artem Baikov <43296143+senconscious@users.noreply.github.com>
2023-01-15 16:42:58 +0300

    [#1] added credo and dialyzer for code check
    * [#1] Added credo for code check
    
    * [#1] Added dialyzer for code check

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule Paymee.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -55,7 +57,8 @@ defmodule Paymee.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "code.check": ["credo", "dialyzer"]
     ]
   end
 end
